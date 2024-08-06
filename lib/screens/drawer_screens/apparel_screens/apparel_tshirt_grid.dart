b26f00da91245df7b13a93964004f2305a8cd2b3
Muhammad Umer Mujtaba <umermujtaba16@gmail.com>
2024-08-06 20:25:01 +0500

    Implemented category grids

---
+++ lib/screens/drawer_screens/apparel_screens/apparel_tshirt_grid.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import '../../../components/category_products.dart';
+import '../../../model/product_model.dart';
+
+class ApparelTshirtGridPage  extends StatelessWidget {
+  const ApparelTshirtGridPage ({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: Icon(
+          Icons.arrow_back_ios_new,
+          color: Colors.black,
+        ),
+        title: const Column(
+          children: [
+            Text(
+              'Open',
+              style: TextStyle(
+                  fontFamily: 'TenorSans', color: Colors.black, fontSize: 22),
+            ),
+            Text(
+              'Fashion',
+              style: TextStyle(
+                  fontFamily: 'TenorSans', color: Colors.black, fontSize: 24),
+            ),
+          ],
+        ),
+        // leading: IconButton(onPressed: () {}, icon: Icon(Icons.menu)),
+        centerTitle: true,
+        actions: [
+          IconButton(onPressed: () {}, icon: const Icon(Icons.search_outlined)),
+          IconButton(
+              onPressed: () {}, icon: const Icon(Icons.shopping_bag_outlined)),
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Row(
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.only(left: 10),
+                      child: Text(
+                        '4500 Apparel',
+                        style: TextStyle(fontFamily: 'TenorSans'),
+                      ),
+                    ),
+                  ],
+                ),
+                Row(
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                        color: Colors.grey.shade300, // Background color
+                        shape: BoxShape.circle, // Circular shape
+                      ),
+                      width: 40, // Diameter of the circle (2 * radius)
+                      height: 40, // Diameter of the circle (2 * radius)
+                      child: const Icon(
+                        Icons.list_sharp,
+                        color: Colors.black,
+                      ),
+                    ),
+                    const SizedBox(
+                      width: 8,
+                    ),
+                    Container(
+                      decoration: BoxDecoration(
+                        color: Colors.grey.shade300, // Background color
+                        shape: BoxShape.circle, // Circular shape
+                      ),
+                      width: 40, // Diameter of the circle (2 * radius)
+                      height: 40, // Diameter of the circle (2 * radius)
+                      child: const Icon(
+                        Icons.table_rows,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
+            SizedBox(
+              height: MediaQuery.of(context)
+                  .size
+                  .height, // Ensure grid takes up available space
+              child: ProductGrid(products: TshirtProducts,),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
